c222b7339197a1375977084dad556b9597b2cd58
Ivy Nguyen <animeweirdo24@gmail.com>
2022-05-11 20:29:39 -0600

    work on book.js

--- src/book.js
+++ src/book.js
@@ -1,11 +1,26 @@
-function createTitle() {
-  
+function createTitle(bookIdea) {
+  return `The ${bookIdea}`
+}
+
+function buildMainCharacter(name, age, pronouns) {
+  return {
+    name: name,
+    age: age,
+    pronouns: pronouns,
+  }
+}
+
+function saveReview(reviews) {
+  return [
+    console.log(reviews.length)
+    .push()
+  ]
 }
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
+  buildMainCharacter,
+  saveReview,
   // calculatePageCount,
   // writeBook,
   // editBook
